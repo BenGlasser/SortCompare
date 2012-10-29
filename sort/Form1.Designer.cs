bc630340d4c6625cc7b54a23130ec797fbac6ef0
Ben Glasser <soul_trip@hotmail.com>
2012-10-28 22:07:44 -0700

    first commit

---
+++ sort/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace sort
+{
+    partial class SortCompare
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SortCompare));
+            this.insertionButton = new System.Windows.Forms.Button();
+            this.selectionButton = new System.Windows.Forms.Button();
+            this.bubbleButton = new System.Windows.Forms.Button();
+            this.mixUpButton = new System.Windows.Forms.Button();
+            this.outBox = new System.Windows.Forms.ListBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.binaryButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // insertionButton
+            // 
+            this.insertionButton.Location = new System.Drawing.Point(13, 234);
+            this.insertionButton.Name = "insertionButton";
+            this.insertionButton.Size = new System.Drawing.Size(126, 41);
+            this.insertionButton.TabIndex = 1;
+            this.insertionButton.Text = "Insertion";
+            this.insertionButton.UseVisualStyleBackColor = true;
+            this.insertionButton.Click += new System.EventHandler(this.insertionButton_Click);
+            // 
+            // selectionButton
+            // 
+            this.selectionButton.Location = new System.Drawing.Point(13, 281);
+            this.selectionButton.Name = "selectionButton";
+            this.selectionButton.Size = new System.Drawing.Size(126, 41);
+            this.selectionButton.TabIndex = 2;
+            this.selectionButton.Text = "Selection";
+            this.selectionButton.UseVisualStyleBackColor = true;
+            this.selectionButton.Click += new System.EventHandler(this.selectionButton_Click);
+            // 
+            // bubbleButton
+            // 
+            this.bubbleButton.Location = new System.Drawing.Point(13, 328);
+            this.bubbleButton.Name = "bubbleButton";
+            this.bubbleButton.Size = new System.Drawing.Size(126, 41);
+            this.bubbleButton.TabIndex = 3;
+            this.bubbleButton.Text = "Bubble";
+            this.bubbleButton.UseVisualStyleBackColor = true;
+            this.bubbleButton.Click += new System.EventHandler(this.bubbleButton_Click);
+            // 
+            // mixUpButton
+            // 
+            this.mixUpButton.ForeColor = System.Drawing.Color.Red;
+            this.mixUpButton.Location = new System.Drawing.Point(13, 13);
+            this.mixUpButton.Name = "mixUpButton";
+            this.mixUpButton.Size = new System.Drawing.Size(125, 113);
+            this.mixUpButton.TabIndex = 7;
+            this.mixUpButton.Text = "Mix Up!";
+            this.mixUpButton.UseVisualStyleBackColor = true;
+            this.mixUpButton.Click += new System.EventHandler(this.mixUpButton_Click);
+            // 
+            // outBox
+            // 
+            this.outBox.FormattingEnabled = true;
+            this.outBox.Location = new System.Drawing.Point(12, 533);
+            this.outBox.Name = "outBox";
+            this.outBox.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.outBox.Size = new System.Drawing.Size(642, 69);
+            this.outBox.TabIndex = 8;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(144, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(510, 510);
+            this.pictureBox.TabIndex = 9;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // binaryButton
+            // 
+            this.binaryButton.Location = new System.Drawing.Point(13, 376);
+            this.binaryButton.Name = "binaryButton";
+            this.binaryButton.Size = new System.Drawing.Size(126, 41);
+            this.binaryButton.TabIndex = 10;
+            this.binaryButton.Text = "Binary";
+            this.binaryButton.UseVisualStyleBackColor = true;
+            this.binaryButton.Click += new System.EventHandler(this.binaryButton_Click);
+            // 
+            // SortCompare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(675, 612);
+            this.Controls.Add(this.binaryButton);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.outBox);
+            this.Controls.Add(this.mixUpButton);
+            this.Controls.Add(this.bubbleButton);
+            this.Controls.Add(this.selectionButton);
+            this.Controls.Add(this.insertionButton);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SortCompare";
+            this.Text = "Sorting Algorithms";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button insertionButton;
+        private System.Windows.Forms.Button selectionButton;
+        private System.Windows.Forms.Button bubbleButton;
+        private System.Windows.Forms.Button mixUpButton;
+        private System.Windows.Forms.ListBox outBox;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button binaryButton;
+    }
+}
+
---
+++ sort/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.Threading;
+namespace sort
+{
+    public partial class SortCompare : Form 
+    {
+        const int _gSZ = 100;
+        Random myRan = new Random();
+        private Rectangle[] myRects = new Rectangle[_gSZ];
+        private int comparisons = 0;
+        private bool busy = false;
+
+        public SortCompare()
+        {
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
+            InitializeComponent();
+            buildArr();
+            pictureBox.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            for (int i = 0; i < _gSZ; i++)
+            {
+                g.FillRectangle(Brushes.Black, myRects[i]);
+            }
+
+        }
+        private void buildArr()
+        {
+            for (int i = 0; i < _gSZ; i++ )
+            {
+                myRects[i] = new Rectangle(new Point(5, (i+1) * 5), new Size((i+1) * 5, 4));
+            }
+        }
+
+        private void mixUpButton_Click(object sender, EventArgs e)
+        {
+            int index1, index2;
+            Point temp;
+            bool[] chosen = new bool[_gSZ];
+            for (int i = 0; i < _gSZ; i++)
+            {
+                index1 = myRan.Next(_gSZ);
+                index2 = myRan.Next(_gSZ);
+                while (chosen[index1]) { index1 = myRan.Next(_gSZ); }
+                if(!(chosen[index1] && chosen[index2]))
+                {
+                    chosen[index1] = true;
+                    temp = myRects[index1].Location;
+                    myRects[index1].Location = myRects[index2].Location;
+                    myRects[index2].Location = temp;
+                }
+                pictureBox.Invalidate();
+            }
+
+        }
+
+        private void insertionSort()
+        {
+            comparisons = 0;
+            DateTime start = DateTime.Now;
+            for (int i = 1; i < _gSZ; i++)
+            {
+                for (int j = i; j > 0 && myRects[j].Location.Y < myRects[j-1].Location.Y && addCompare(); j--)
+                {
+                    swap(j, j - 1);
+                    pictureBox.Invalidate();
+                    Thread.Sleep(5);
+                }
+            }
+            DateTime end = DateTime.Now;
+            if (outBox.InvokeRequired)
+            {
+                outBox.Invoke(
+                  new ThreadStart(delegate
+                  {
+                      outBox.Items.Insert(0, "Insertion Sort Finished In: " + (end - start).TotalSeconds + " Seconds and Made " + comparisons + " Comparisons");
+                }));
+            }
+            else
+            {
+                outBox.Items.Insert(0, "Insertion Sort Finished In: " + (end - start).TotalSeconds + " Seconds and Made " + comparisons + " Comparisons");
+            }
+        }
+        private void selectionSort()
+        {
+            comparisons = 0;
+            DateTime start = DateTime.Now;
+            for (int i = 0; i < _gSZ; i++)
+            {
+                int k = i;
+                for (int j = i + 1; j < _gSZ; j++)
+                {
+                    if (myRects[j].Location.Y < myRects[k].Location.Y && addCompare())
+                    {
+                        k = j;
+                        pictureBox.Invalidate();
+                        Thread.Sleep(5);
+                    }
+                }
+                swap(i, k);
+                pictureBox.Invalidate();
+            }
+            DateTime end = DateTime.Now;
+            if (outBox.InvokeRequired)
+            {
+                outBox.Invoke(
+                  new ThreadStart(delegate
+                  {
+                      outBox.Items.Insert(0, "Selection Sort Finished In: " + (end - start).TotalSeconds + " Seconds and Made " + comparisons + " Comparisons");
+                  }));
+            }
+            else
+            {
+                outBox.Items.Insert(0, "Selection Sort Finished In: " + (end - start).TotalSeconds + " Seconds and Made " + comparisons + " Comparisons");
+            }
+        }
+        private void bubble()
+        {
+            comparisons = 0;
+            DateTime start = DateTime.Now;
+            for (int i = -1; i < _gSZ; i++)
+            {
+                bool swapped = false;
+                for (int j = _gSZ-1; j > i + 1; j--)
+                {
+                    if (myRects[j].Location.Y < myRects[j-1].Location.Y && addCompare())
+                    {
+                        swap(j, j - 1);
+                        pictureBox.Invalidate();
+                        Thread.Sleep(5);
+                        swapped = true;
+                    }
+                }
+                if (!swapped) break;
+            }
+            DateTime end = DateTime.Now;
+            if (outBox.InvokeRequired)
+            {
+                outBox.Invoke(
+                  new ThreadStart(delegate
+                  {
+                      outBox.Items.Insert(0, "Bubble Sort Finished In: " + (end - start).TotalSeconds + " Seconds and Made " + comparisons + " Comparisons");
+                  }));
+            }
+            else
+            {
+                outBox.Items.Insert(0, "Bubble Sort Finished In: " + (end - start).TotalSeconds + " Seconds and Made " + comparisons + " Comparisons");
+            }
+        }
+        private void merge(int[] myArr)
+        {
+            comparisons = 0;
+            DateTime start = DateTime.Now;
+            //split in half
+        }
+        private void binary()
+        {
+            DateTime start = DateTime.Now;
+            comparisons = 0;
+            for (int i = 1; i < myRects.Length; ++i)
+            {
+                Rectangle temp = myRects[i];
+                int left = 0;
+                int right = i;
+                while (left < right)
+                {
+                    int middle = (left + right) / 2;
+                    if (temp.Location.Y > myRects[middle].Location.Y && addCompare())
+                        left = middle + 1;
+                    else
+                        right = middle;
+                }// end while
+                for (int j = i; j > left; j--)
+                {
+                    swap(j, j - 1);
+                    pictureBox.Invalidate();
+                    Thread.Sleep(5);
+
+                }
+            }//end for
+            DateTime end = DateTime.Now;
+            if (outBox.InvokeRequired)
+            {
+                outBox.Invoke(
+                  new ThreadStart(delegate
+                  {
+                      outBox.Items.Insert(0, "Binary Sort Finished In: " + (end - start).TotalSeconds + " Seconds and Made " + comparisons + " Comparisons");
+                  }));
+            }
+            else
+            {
+                outBox.Items.Insert(0, "Binary Sort Finished In: " + (end - start).TotalSeconds + " Seconds and Made " + comparisons + " Comparisons");
+            }
+        }// end binary
+        private void swap(int i, int j)
+        {
+            Point temp;
+            temp = myRects[i].Location;
+            myRects[i].Location = myRects[j].Location;
+            myRects[j].Location = temp;
+            
+        }
+        private bool addCompare()
+        {
+            comparisons++;
+            return true;
+        }
+
+        private void insertionButton_Click(object sender, EventArgs e)
+        {
+            if (!busy)
+            {
+                busy = true;
+                Thread t = new Thread(new ThreadStart(this.insertionSort));
+                t.Start();
+            }
+            busy = false;
+        }
+        private void selectionButton_Click(object sender, EventArgs e)
+        {
+            if (!busy)
+            {
+                busy = true;
+                Thread t = new Thread(new ThreadStart(this.selectionSort));
+                t.Start();
+            }
+            busy = false;
+        }
+
+        private void bubbleButton_Click(object sender, EventArgs e)
+        {
+            if (!busy)
+            {
+                busy = true;
+                Thread t = new Thread(new ThreadStart(this.bubble));
+                t.Start();
+            }
+            busy = false;
+        }
+
+        private void binaryButton_Click(object sender, EventArgs e)
+        {
+            if (!busy)
+            {
+                busy = true;
+                Thread t = new Thread(new ThreadStart(this.binary));
+                t.Start();
+            }
+            busy = false;
+        }
+
+    }
+
+}
